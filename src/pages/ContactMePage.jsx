88b0ff4e072013be55ffa6c1371248dbe07aeef4
Thiago <thiago.wander22@gmail.com>
2024-06-16 19:32:45 -0300

    :art: added anchors to contact icons

--- src/pages/ContactMePage.jsx
+++ src/pages/ContactMePage.jsx
@@ -20,20 +20,26 @@ const ContactMePage = () => {
             </div>
             <div className="contact-page-container">
                 <div className="contact-cards-grid">
-                    <div className="contact-card">
-                        <img src={linkedinIcon} className="contact-icon" />
-                    </div>
-                    <div className="contact-card">
-                        <img src={githubIcon} className="contact-icon" />
-                    </div>
-                    <div className="contact-card">
-                        <img src={emailIcon} className="contact-icon" />
-                    </div>
-                </div>
+                    <a href="https://www.linkedin.com/in/thiagoamorim1/" target="_blank" rel="noopener noreferrer">
+                        <div className="contact-card">
+                            <img src={linkedinIcon} className="contact-icon" />
+                        </div>
+                    </a>
+                    <a href="https://github.com/thiago186" target="_blank" rel="noopener noreferrer">
+                        <div className="contact-card">
+                            <img src={githubIcon} className="contact-icon" />
+                        </div>
+                    </a>
+                    <a href="mailto:thiago.wander22@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <div className="contact-card">
+                            <img src={emailIcon} className="contact-icon" />
+                        </div>
+                    </a>
+                </div >
                 <div className='cmpvline cmpvline1' />
                 <div className='cmpvline cmpvline2' />
                 <div className='cmpvline cmpvline3' />
-            </div>
+            </div >
             <div className='cmphline cmphline1' />
             <div className='cmphline cmphline2' />
             <div className='cmphline cmphline3' />
