c278db92eff95be4d260ef16aafa61d9fbf0006e
Thiago <thiago.wander22@gmail.com>
2024-06-16 19:13:03 -0300

    🚧 contact me page

--- src/pages/ContactMePage.jsx
+++ src/pages/ContactMePage.jsx
@@ -18,21 +18,22 @@ const ContactMePage = () => {
             <div className='contact-me-title-container'>
                 <Title content="Contact Me" />
             </div>
-
-            <div className="contact-cards-grid">
-                <div className="contact-card">
-                    <img src={linkedinIcon} className="contact-icon" />
+            <div className="contact-page-container">
+                <div className="contact-cards-grid">
+                    <div className="contact-card">
+                        <img src={linkedinIcon} className="contact-icon" />
+                    </div>
+                    <div className="contact-card">
+                        <img src={githubIcon} className="contact-icon" />
+                    </div>
+                    <div className="contact-card">
+                        <img src={emailIcon} className="contact-icon" />
+                    </div>
                 </div>
-                <div className="contact-card">
-                    <img src={githubIcon} className="contact-icon" />
-                </div>
-                <div className="contact-card">
-                    <img src={emailIcon} className="contact-icon" />
-                </div>
+                <div className='cmpvline cmpvline1' />
+                <div className='cmpvline cmpvline2' />
+                <div className='cmpvline cmpvline3' />
             </div>
-            <div className='cmpvline cmpvline1' />
-            <div className='cmpvline cmpvline2' />
-            <div className='cmpvline cmpvline3' />
             <div className='cmphline cmphline1' />
             <div className='cmphline cmphline2' />
             <div className='cmphline cmphline3' />
